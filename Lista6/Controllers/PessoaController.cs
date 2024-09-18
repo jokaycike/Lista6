b5cf5a3094d88028d62f3e536141fc8243093b6e
jokaycike <joana-luizasilva@hotmail.com>
2024-09-17 21:13:47 -0300

    Add project files.

---
+++ Lista6/Controllers/PessoaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lista6.Controllers
+{
+    [ApiController]
+    [Route("api/aluno")]
+    public class PessoaController : Controller
+    {
+        private 
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
